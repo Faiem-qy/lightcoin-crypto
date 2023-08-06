6040d8da088de3cee9bdd7765b4f77a3ba4e9089
Faiem-qy <f.quanyoung@gmail.com>
2023-08-05 23:11:36 -0400

    added deposit and account classes

--- index.js
+++ index.js
@@ -5,18 +5,39 @@ class Withdrawal {
   constructor(amount) {
     this.amount = amount;
   }
-
+  
   commit() {
     balance -= this.amount;
+  }
+  
+}
+
+class Deposit {
+
+  constructor(amount) {
+    this.amount = amount;
+  }
+
+  commit() {
+    balance += this.amount;
   }
 
 }
 
+class Account {
 
+  constructor(username) {
+    this.username = username;
+    // Have the account balance start at $0 since that makes more sense.
+    this.balance = 0;
+  }
 
+}
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
+
+const myAccount = new Account("snow-patrol");
 
 t1 = new Withdrawal(50.25);
 t1.commit();
@@ -27,3 +48,9 @@ t2.commit();
 console.log('Transaction 2:', t2);
 
 console.log('Balance:', balance);
+
+
+
+t3 = new Deposit(120.00);
+t3.commit();
+console.log('Transaction 3:', t3);
